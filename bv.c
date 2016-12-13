f14630ba94a505cca8f4ecc44c4f17dcb1887b0f
sam-zheng <sam.zheng.x@gmail.com>
2016-12-14 00:29:48 +0800

    aligned view

--- bv.c
+++ bv.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 int main(int argc, char** argv) {
   if (argc <= 1) {
      printf("please specify a file\n");
@@ -7,16 +8,24 @@ int main(int argc, char** argv) {
   if (aFile == NULL) {
      printf("error opening file: %s", argv[0]);
   }
+  fseek(aFile, 0L, SEEK_END);
+  long aSize = ftell(aFile);
+  char aTemp[1024];
+  rewind(aFile);
+  long aLines = aSize & ~15; 
+  sprintf(aTemp, "%lx", aLines);
+  int aLen = strlen(aTemp);
+
   int c;
   int n = 0;
-  printf("%8s", "");
+  printf("%*s+    ", aLen - 1, "");
   for (int i = 0; i < 16; i++) {
     printf("%02x  ", i);
   }
-  printf("\n");
+  printf("\n\n");
   while ((c = fgetc(aFile)) != -1) {
     if (n % 16 == 0) {
-      printf("%6x  ", n);
+      printf("%*x    ", aLen, n);
     }
     printf("%02x  ", c);
     n++;
