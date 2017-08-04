1c8e9a4281e128b608a8ce310a4c3c0dbc4157d7
sam-zheng <sam.zheng.x@gmail.com>
2017-08-04 11:55:05 +0800

    tidy up

--- bv.c
+++ bv.c
@@ -3,25 +3,25 @@
 #include <ctype.h>
 int main(int argc, char** argv) {
   if (argc <= 1) {
-     printf("please specify a file\n");
-	 return -1;
+    fprintf(stderr, "please specify a file\n");
+    return 1;
   }
   FILE* aFile = fopen(argv[1], "r");
   if (aFile == NULL) {
-     printf("error opening file: %s", argv[0]);
-	 return -1;
+    fprintf(stderr, "error opening file: %s\n", argv[1]);
+    return 1;
   }
   fseek(aFile, 0L, SEEK_END);
   long aSize = ftell(aFile);
   char aTemp[1024];
   rewind(aFile);
-  long aLines = aSize & ~15; 
-  sprintf(aTemp, "%lx", aLines);
-  int aLen = strlen(aTemp);
+
+  sprintf(aTemp, "%lx", (aSize & ~15));
+  int aHW = strlen(aTemp);
 
   int c;
   int n = 0;
-  printf("%*s+    ", aLen - 1, "");
+  printf("%*s+    ", aHW - 1, "");
   for (int i = 0; i < 16; i++) {
     printf("%02x  ", i);
   }
@@ -31,7 +31,7 @@ int main(int argc, char** argv) {
   int i = 0;
   while ((c = fgetc(aFile)) != -1) {
     if (n % 16 == 0) {
-      printf("%*x    ", aLen, n);
+      printf("%*x    ", aHW, n);
     }
     printf("%02x  ", c);
     if (isprint(c)) {
